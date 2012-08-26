461af742b10c4e2b2f870068850948550b3c2502
moguno <moguno@Akira.(none)>
2012-08-27 00:28:45 +0900

    スクリーンネームにに検索ワードが入っているメッセージを除外するようにした。 大文字小文字を無視するようにした。

--- kininarimasu.rb
+++ kininarimasu.rb
@@ -38,16 +38,15 @@ Plugin.create :kininarimasu do
     input("プレフィックス", :interest_prefix)
   end 
   
-  
-  # 検索キーワード文字列の生成
-  def get_query_keyword()
+
+  # キーワードのリストを取得
+  def get_keywords()
     [:interest_keyword1, :interest_keyword2, :interest_keyword3, :interest_keyword4, :interest_keyword5]
     .select{ |key| !UserConfig[key].empty? }
     .map{ |key| UserConfig[key] }
-    .join("+OR+")
   end
-  
-  
+
+
   # 検索用ループ
   def search_loop(service)
     Reserver.new(UserConfig[:interest_period]){
@@ -72,13 +71,14 @@ Plugin.create :kininarimasu do
   
           msg.user[:created] ||= Time.now
 
+          # タイムラインに登録
           if defined?(timeline)
             timeline(:home_timeline) << [msg]
           else
             Plugin.call(:update, service, [msg])
           end
 
-          #  p "last message :" + $result_queue.size.to_s
+          puts "last message :" + $result_queue.size.to_s
         end
 
         insert_loop service
@@ -90,16 +90,10 @@ Plugin.create :kininarimasu do
   end
   
 
-  # 起動時処理
-  on_boot do |service|
-    search_loop service
-    insert_loop service
-  end
-  
-  
   # 検索
   def search_keyword(service)
-    query_keyword = get_query_keyword()
+    keywords = get_keywords() 
+    query_keyword = keywords.join("+OR+")
   
     # p query_keyword
   
@@ -109,7 +103,7 @@ Plugin.create :kininarimasu do
   
     params = {}
   
-    params[:q] = query_keyword
+    params[:q] = query_keyword + "+-rt+-via"
     params[:rpp] = "100"
   
     if UserConfig[:interest_japanese] then
@@ -126,16 +120,27 @@ Plugin.create :kininarimasu do
         $last_keyword = query_keyword
   
         res = res.select { |es|
+          result_tmp = false
+
           if es[:message] =~ /^RT / then
-            false
+            result_tmp = false
+          elsif $last_time == nil then
+            result_tmp = true
+          elsif $last_time < Time.parse(es[:created_at]) then
+            result_tmp = true
           else
-            if $last_time == nil then
-              true
-            elsif $last_time < Time.parse(es[:created_at]) then
+            result_tmp = false
+          end
+
+          # 重たい検索を行う
+          if result_tmp then
+            if keywords.inject(false) {|result, key| result | (/#{key}/i =~ es[:message])} then
               true
             else
               false
             end 
+          else
+            false
           end
         }
   
@@ -159,5 +164,12 @@ Plugin.create :kininarimasu do
         puts e.backtrace
       end
     }
+  end
+
+
+  # 起動時処理
+  on_boot do |service|
+    search_loop service
+    insert_loop service
   end
 end
