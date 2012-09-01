d875910f6c43348186659661e7fb3a58a57eea36
moguno <moguno@Akira.(none)>
2012-09-02 00:29:11 +0900

    1キーワードにつき1回検索することにした。

--- kininarimasu.rb
+++ kininarimasu.rb
@@ -1,44 +1,22 @@
 # -*- coding: utf-8 -*-
   
 require 'date'
-  
-Plugin.create :kininarimasu do 
-  
-  # コンフィグの初期化
-  UserConfig[:interest_keyword1] ||= ""
-  UserConfig[:interest_keyword2] ||= ""
-  UserConfig[:interest_keyword3] ||= ""
-  UserConfig[:interest_keyword4] ||= ""
-  UserConfig[:interest_keyword5] ||= ""
-  UserConfig[:interest_japanese] ||= true
-  UserConfig[:interest_period] ||= 60
-  UserConfig[:interest_insert_period] ||= 3
-  UserConfig[:interest_prefix] ||= ""
-  
 
-  # グローバル変数の初期化
-  $result_queue = []
-  $last_time = nil
-  $last_keyword = ""
-  $queue_lock = Mutex.new()
+# 検索クラス
+class Chitanda
+  attr_reader :last_fetch_time,:keywords
 
 
-  # 設定画面
-  settings "わたし、気になります" do
-    settings "検索ワード" do
-      input("", :interest_keyword1)
-      input("", :interest_keyword2)
-      input("", :interest_keyword3)
-      input("", :interest_keyword4)
-      input("", :interest_keyword5)
-    end
- 
-    boolean("日本語のツイートのみ", :interest_japanese)
-    adjustment("ポーリング間隔（秒）", :interest_period, 1, 6000)
-    adjustment("混ぜ込み間隔（秒）", :interest_insert_period, 1, 600)
-    input("プレフィックス", :interest_prefix)
-  end 
-  
+  # コンストラクタ
+  def initialize(service, keywords)
+    @service = service
+    @keywords = keywords
+    @result_queue = Array.new()
+    @queue_lock = Mutex.new()
+    @last_result_time = nil
+    @last_fetch_time = Time.now
+  end
+
 
   # 日時文字列をパースする
   def parse_time(str)
@@ -50,67 +28,26 @@ Plugin.create :kininarimasu do
   end
 
 
-  # キーワードのリストを取得
-  def get_keywords()
-    [:interest_keyword1, :interest_keyword2, :interest_keyword3, :interest_keyword4, :interest_keyword5]
-    .select{ |key| !UserConfig[key].empty? }
-    .map{ |key| UserConfig[key] }
+  # 検索結果を取り出す
+  def fetch()
+    msg = nil
+
+    @queue_lock.synchronize {
+      msg = @result_queue.shift
+    }
+
+    if msg != nil then
+      @last_fetch_time = Time.now
+    end
+
+    return msg
   end
 
 
-  # 検索用ループ
-  def search_loop(service)
-    Reserver.new(UserConfig[:interest_period]){
-      keywords = search_keyword(service) 
-      search_loop service
-    } 
-  end
-  
-  
-  # 混ぜ込みループ
-  def insert_loop(service)
-    Reserver.new(UserConfig[:interest_insert_period]){
-      begin
-        msg = nil
+  # 検索する
+  def search()
+    query_keyword = @keywords.join("+OR+")
 
-        $queue_lock.synchronize {
-          msg = $result_queue.shift
-        }
-
-        if msg != nil then
-
-          msg[:created] = Time.now
-  
-          if !UserConfig[:interest_prefix].empty? then
-            msg[:message] = UserConfig[:interest_prefix] + " " + msg[:message]
-          end
-  
-          msg.user[:created] ||= Time.now
-
-          # タイムラインに登録
-          if defined?(timeline)
-            timeline(:home_timeline) << [msg]
-          else
-            Plugin.call(:update, service, [msg])
-          end
-
-          # puts "last message :" + $result_queue.size.to_s
-        end
-
-        insert_loop service
-
-      rescue=>e
-        puts e.backtrace
-      end
-    } 
-  end
-  
-
-  # 検索
-  def search_keyword(service)
-    keywords = get_keywords() 
-    query_keyword = keywords.join("+OR+")
-  
     # p query_keyword
   
     if query_keyword.empty? then
@@ -125,19 +62,9 @@ Plugin.create :kininarimasu do
     if UserConfig[:interest_japanese] then
       params[:lang] = "ja"
     end
-  
-    service.search(params).next{ |res| 
-      begin
-        if $last_keyword != query_keyword then
-          $last_time = nil
 
-          $queue_lock.synchronize {
-            $result_queue.clear
-          }
-        end
-  
-        $last_keyword = query_keyword
-  
+    @service.search(params).next{ |res| 
+      begin
         res = res.select { |es|
           result_tmp = false
 
@@ -172,21 +99,147 @@ Plugin.create :kininarimasu do
         res.each { |es| 
           tim = parse_time(es[:created_at])
   
-          if tim != nil && ($last_time == nil || $last_time < tim) then
-            $last_time = tim
+          if tim != nil && (@last_result_time == nil || @last_result_time < tim) then
+            @last_result_time = tim
           end
         }
   
         # p "new message:" + res.size.to_s
         # p "last time:" + $last_time.to_s
   
-        $queue_lock.synchronize {
-          $result_queue.concat(res.reverse)
+        @queue_lock.synchronize {
+          @result_queue.concat(res.reverse)
         }
       rescue => e
+        puts e
         puts e.backtrace
       end
     }
+  end
+end
+  
+
+Plugin.create :kininarimasu do 
+  
+  # コンフィグの初期化
+  UserConfig[:interest_keyword1] ||= ""
+  UserConfig[:interest_keyword2] ||= ""
+  UserConfig[:interest_keyword3] ||= ""
+  UserConfig[:interest_keyword4] ||= ""
+  UserConfig[:interest_keyword5] ||= ""
+  UserConfig[:interest_japanese] ||= true
+  UserConfig[:interest_period] ||= 60
+  UserConfig[:interest_insert_period] ||= 3
+  UserConfig[:interest_prefix] ||= ""
+  
+
+  # グローバル変数の初期化
+  $chitandas = []
+
+
+  # 設定画面
+  settings "わたし、気になります" do
+    settings "検索ワード" do
+      input("", :interest_keyword1)
+      input("", :interest_keyword2)
+      input("", :interest_keyword3)
+      input("", :interest_keyword4)
+      input("", :interest_keyword5)
+    end
+ 
+    boolean("日本語のツイートのみ", :interest_japanese)
+    adjustment("ポーリング間隔（秒）", :interest_period, 1, 6000)
+    adjustment("混ぜ込み間隔（秒）", :interest_insert_period, 1, 600)
+    input("プレフィックス", :interest_prefix)
+  end 
+  
+
+  # キーワードのリストを取得
+  def get_keywords()
+    [:interest_keyword1, :interest_keyword2, :interest_keyword3, :interest_keyword4, :interest_keyword5]
+    .map{ |key| UserConfig[key] }
+  end
+
+
+  # 検索用ループ
+  def search_loop(service)
+    Reserver.new(UserConfig[:interest_period]){
+      keywords = search_keyword(service) 
+      search_loop service
+    } 
+  end
+  
+  
+  # 混ぜ込みループ
+  def insert_loop(service)
+    Reserver.new(UserConfig[:interest_insert_period]){
+      begin
+        fetch_order = $chitandas.select(){ |a| a != nil }.sort() { |a, b|
+          a.last_fetch_time <=> b.last_fetch_time
+        }
+
+        msg = nil
+
+        fetch_order.each { |chitanda|
+          msg = chitanda.fetch()
+
+          if msg != nil then
+            break
+          end
+        }
+
+        if msg != nil then
+
+          msg[:created] = Time.now
+  
+          if !UserConfig[:interest_prefix].empty? then
+            msg[:message] = UserConfig[:interest_prefix] + " " + msg[:message]
+          end
+  
+          msg.user[:created] ||= Time.now
+
+          # タイムラインに登録
+          if defined?(timeline)
+            timeline(:home_timeline) << [msg]
+          else
+            Plugin.call(:update, service, [msg])
+          end
+
+          # puts "last message :" + $result_queue.size.to_s
+        end
+
+      rescue => e
+        puts e
+        puts e.backtrace
+
+      ensure
+        insert_loop service
+
+      end
+    } 
+  end
+  
+
+  # 検索
+  def search_keyword(service)
+    begin
+      keywords = get_keywords() 
+
+      (0..keywords.length - 1).each { |i|
+        if keywords[i].empty? then
+          $chitandas[i] = nil
+        elsif ($chitandas[i] == nil) || ($chitandas[i].keywords <=> [keywords[i]]) != 0 then
+          $chitandas << Chitanda.new(service, [keywords[i]])
+        end
+      }
+
+      $chitandas.select { |a| a != nil }.each { |chitanda|
+        chitanda.search()
+      }
+    rescue => e
+      puts e
+      puts e.backtrace
+    end
   end
 
 
