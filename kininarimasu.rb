2cdc627ee08a4e404ebf2c7dd6999f7db446167d
moguno <moguno@Akira.(none)>
2012-09-12 00:48:21 +0900

    混ぜ込むツイートの時刻を維持するようにした。

--- kininarimasu.rb
+++ kininarimasu.rb
@@ -225,14 +225,16 @@ Plugin.create :kininarimasu do
 
         if msg != nil then
 
-          msg[:created] = Time.now
+#          msg[:created] = Time.now
+          msg[:modified] = Time.now
           msg[:kininarimasu] = true
   
           if !UserConfig[:interest_prefix].empty? then
             msg[:message] = UserConfig[:interest_prefix] + " " + msg[:message]
           end
   
-          msg.user[:created] ||= Time.now
+#          msg.user[:created] ||= Time.now
+          msg.user[:modified] ||= Time.now
 
           # タイムラインに登録
           if defined?(timeline)
