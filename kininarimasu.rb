28278bae6ebb064c4555f913572f299ec5ac9357
moguno <moguno@Akira.(none)>
2012-09-12 00:55:33 +0900

    検索結果のツイート時刻を維持するようにした。

--- kininarimasu.rb
+++ kininarimasu.rb
@@ -216,14 +216,14 @@ Plugin.create :kininarimasu do
 
         if msg != nil then
 
-          msg[:created] = Time.now
+          msg[:modified] = Time.now
           msg[:kininarimasu] = true
   
           if !UserConfig[:interest_prefix].empty? then
             msg[:message] = UserConfig[:interest_prefix] + " " + msg[:message]
           end
   
-          msg.user[:created] ||= Time.now
+          msg.user[:modified] ||= Time.now
 
           # タイムラインに登録
           if defined?(timeline)
